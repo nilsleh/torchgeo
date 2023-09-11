04896db0f0d0d82318a953fd014c2c87958ff222
Nils Lehmann <nils.lehmann24@gmail.com>
2023-09-11 14:28:02 +0200

    batch time sampler

--- torchgeo/samplers/batch.py
+++ torchgeo/samplers/batch.py
@@ -4,12 +4,16 @@
 """TorchGeo batch samplers."""
 
 import abc
+import random
 from collections.abc import Iterator
+from datetime import datetime, timedelta
 from typing import Optional, Union
 
 import torch
 from rtree.index import Index, Property
 from torch.utils.data import Sampler
+
+from torchgeo.datasets import BoundingBox, GeoDataset
 
 from ..datasets import BoundingBox, GeoDataset
 from .constants import Units
@@ -122,13 +126,13 @@ class RandomBatchGeoSampler(BatchGeoSampler):
                     self.length += 1
                 self.hits.append(hit)
                 areas.append(bounds.area)
-        if length is not None:
-            self.length = length
 
         # torch.multinomial requires float probabilities > 0
         self.areas = torch.tensor(areas, dtype=torch.float)
         if torch.sum(self.areas) == 0:
             self.areas += 1
+        if length is not None:
+            self.length = length
 
     def __iter__(self) -> Iterator[list[BoundingBox]]:
         """Return the indices of a dataset.
@@ -157,3 +161,108 @@ class RandomBatchGeoSampler(BatchGeoSampler):
             number of batches in an epoch
         """
         return self.length // self.batch_size
+
+
+class BatchTimeWindowGeoSampler(BatchGeoSampler):
+    """Samples batches of elements from a region and time of interest randomly.
+
+    This is particularly useful during training when you want to maximize the size of
+    the dataset and return as many random :term:`chips <chip>` as possible that vary
+    both across spatial and time extent within the batch. Note that
+    randomly sampled chips may overlap in both the spatial and time dimension.
+    """
+
+    def __init__(
+        self,
+        dataset: GeoDataset,
+        size: Union[tuple[float, float], float],
+        batch_size: int,
+        encoder_length: int,
+        prediction_length: int,
+        time_unit: str,
+        time_delta: int = 0,
+        consecutive: bool = True,
+        time_range: Optional[tuple[datetime, datetime]] = None,
+        roi: Optional[BoundingBox] = None,
+        size_units: Units = Units.PIXELS,
+        max_samples_per_geolocation: int = None,
+        length: Optional[int] = None,
+        units: Units = Units.PIXELS,
+    ) -> None:
+        """Initialize a new Sampler instance.
+
+        Args:
+            dataset: dataset to index from
+            size: dimensions of each :term:`patch`
+            batch_size: number of samples per batch
+            length: number of samples per epoch
+                (defaults to approximately the maximal number of non-overlapping
+                :term:`chips <chip>` of size ``size`` that could be sampled from
+                the dataset)
+            roi: region of interest to sample from (minx, maxx, miny, maxy, mint, maxt)
+                (defaults to the bounds of ``dataset.index``)
+            units: defines if ``size`` is in pixel or CRS units
+
+        .. versionadded:: 0.5
+        """
+        super().__init__(dataset, roi)
+
+        self.size = _to_tuple(size)
+
+        # check time unit
+        assert (
+            time_unit in self.allowed_time_units
+        ), f"Currently, only supporting one of {self.allowed_time_units} as time unit."
+        self.time_unit = time_unit
+        self.encoder_length = encoder_length
+        self.prediction_length = prediction_length
+        self.consecutive = consecutive
+
+        if size_units == Units.PIXELS:
+            self.size = (self.size[0] * self.res, self.size[1] * self.res)
+
+        self.length = 0
+
+        self.hits: list[list[float]] = []
+        areas: list[float] = []
+        for hit in self.index.intersection(tuple(self.roi), objects=True):
+            bounds = BoundingBox(*hit.bounds)
+            if (
+                bounds.maxx - bounds.minx >= self.size[1]
+                and bounds.maxy - bounds.miny >= self.size[0]
+            ):
+                if bounds.area > 0:
+                    rows, cols = tile_to_chips(bounds, self.size)
+                    self.length += rows * cols
+                else:
+                    self.length += 1
+                self.hits.append(hit)
+                areas.append(bounds.area)
+        if length is not None:
+            self.length = length
+
+        # torch.multinomial requires float probabilities > 0
+        self.areas = torch.tensor(areas, dtype=torch.float)
+        if torch.sum(self.areas) == 0:
+            self.areas += 1
+
+        # get all possible subsequence within the time range
+        possible_input_time_ranges = self._compute_subsequences()
+
+        # if there are a lot of possible subsequences
+        # limit them
+        random.shuffle(possible_input_time_ranges)
+        if max_samples_per_geolocation is not None:
+            possible_input_time_ranges = possible_input_time_ranges[
+                :max_samples_per_geolocation
+            ]
+
+        self.possible_input_time_ranges = possible_input_time_ranges
+
+    def __iter__(self) -> Iterator[list[BoundingBox]]:
+        """Return the indices of a dataset.
+
+        Returns:
+            batch of (minx, maxx, miny, maxy, mint, maxt) coordinates to index a dataset
+        """
+        return super().__iter__()
